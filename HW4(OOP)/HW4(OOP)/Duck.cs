e92e82af3de951beeb6623aca3be27a703b8b26c
Yuliia Shyshman <Yuliia_Shyshman@epam.com>
2019-03-14 16:16:35 +0200

    Added hw4(oop)

---
+++ HW4(OOP)/HW4(OOP)/Duck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_OOP_
+{
+    class Duck :Animal,IFlying
+    {
+        public Duck(string name, double weight, int age)
+            : base(name, weight, age)
+        { }
+
+        public override void Eat()
+        {
+            Console.WriteLine($"Duck {Name} eat grass");
+        }
+        public void Fly()
+        {
+            Console.WriteLine($"{Name} weight {Weight} kg but he can fly :)");
+        }
+
+        public override string ToString()
+        {
+            return "Duck - " + base.ToString();
+        }
+    }
+}
---
+++ HW4(OOP)/HW4(OOP)/Straus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_OOP_
+{
+    class Straus : Animal
+    {
+        public Straus(string name, double weight, int age)
+            : base(name, weight, age)
+        { }
+
+        public override void Eat()
+        {
+            Console.WriteLine($"Straus {Name} eat grass and hay");
+        }
+
+        public override string ToString()
+        {
+            return "Straus - " + base.ToString();
+        }
+    }
+}
